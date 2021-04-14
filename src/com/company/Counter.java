c7c38038487aa31e27ddffdf3a160c92bcd6e41d
Mary Kazaryan <82213157+mary-kazaryan@users.noreply.github.com>
2021-04-15 01:14:27 +0300

    Add files via upload

---
+++ src/com/company/Counter.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Counter {
+
+    public static void main(String[] args) {
+        
+        int number = 0;
+
+        while (number >= -45) {
+            System.out.println(number);
+            number = number - 5;
+            }
+        }
+
+
+
+
+    }
